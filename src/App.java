e92f50ce16a67870643954a1b27bc8704125b1f3
yasuo <yasuoisslaying>
2025-04-14 23:40:10 +0200

    Hello trying e!

--- src/App.java
+++ src/App.java
@@ -4,7 +4,16 @@ public class App {
         for(int i=0; i<5; i++){
             System.out.println("Hello "+i);
         }
+
+        System.out.println("The last hello");
         
 
+    
+
+    for(int i=0; i<5; i++){
+        System.out.println("Hello "+i);
     }
+
 }
+}
+
