ee58c655e4259839873c8908d0d7898866ad54cd
yasuo <yasuoisslaying>
2025-04-17 21:58:13 +0200

    added a file

--- src/App.java
+++ src/App.java
@@ -1,6 +1,10 @@
 public class App {
     public static void main(String[] args)  {
 
+        functions func = new functions();
+
+        System.out.println(func.plus(4, 3));
+
         for(int i=0; i<5; i++){
             System.out.println("Hello "+i);
         }
---
+++ src/functions.java
@@ -0,0 +1,5 @@
+public class functions {
+    public int plus(int a , int b){
+        return a+b;
+    }
+}
