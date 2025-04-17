1e2dd4bd6d11d023e3724872e2b0fb4b517665bc
yasuo <yasuoisslaying>
2025-04-17 22:00:58 +0200

    new function

--- src/App.java
+++ src/App.java
@@ -4,6 +4,8 @@ public class App {
         functions func = new functions();
 
         System.out.println(func.plus(4, 3));
+        
+        System.out.println(func.minus(3, 2));
 
         for(int i=0; i<5; i++){
             System.out.println("Hello "+i);
--- src/functions.java
+++ src/functions.java
@@ -2,4 +2,7 @@ public class functions {
     public int plus(int a , int b){
         return a+b;
     }
+    public int minus(int a , int b){
+        return a-b;
+    }
 }
